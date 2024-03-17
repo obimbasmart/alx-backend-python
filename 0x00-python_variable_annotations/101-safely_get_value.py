2a97116f5d395f9f97fd3805da66ffdf8c61f6ef
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-03-17 11:23:33 +0100

    add:  new advanced task

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""Duck typing - first element of a sequence"""
+
+from typing import Mapping, Any, Union, TypeVar
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
+    if key in dct:
+        return dct[key]
+    else:
+        return default
