3a794cd9441309e6be5d4086d9d8acf018c43e38
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-03-17 10:14:06 +0100

    fix: execution error -> syntax error

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,9 +2,9 @@
 
 """Basic annotations - Complex types - mixed list"""
 
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[float | int]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """return sum of floats"""
     return sum(mxd_lst)
