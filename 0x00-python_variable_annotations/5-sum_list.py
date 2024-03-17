17e78163a5c9aadcc54489a3fc4705672ec08f33
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-03-17 10:00:04 +0100

    fix: syntax error -> incorrect iterable

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,7 +2,9 @@
 
 """Basic annotations - Complex types - list of floats"""
 
+from typing import List
 
-def sum_list(input_list: list[float]) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     """return sum of floats"""
     return sum(input_list)
