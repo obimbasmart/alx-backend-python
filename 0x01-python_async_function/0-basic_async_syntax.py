e6736d2637beca3ce16730248ddf7782a420bfcf
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-03-19 23:27:32 +0100

    add: first task - basic of coroutines

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+# /usr/bin/env python3
+
+import asyncio
+from random import uniform
+
+"""Bascs of Async IO"""
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """random delay coroutine"""
+    await_time: float = uniform(0, max_delay)
+    await asyncio.sleep(await_time)
+    return await_time
