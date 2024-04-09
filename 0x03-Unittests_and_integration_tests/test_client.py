53fdbe5cc0fd802a4dfeeeb9de193e09c84f0248
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-04-09 04:39:39 +0100

    test: task 05

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -5,7 +5,8 @@
 import unittest
 from unittest.mock import (
     MagicMock,
-    patch
+    patch,
+    PropertyMock
 )
 from typing import Dict, Callable
 from client import GithubOrgClient
@@ -30,3 +31,11 @@ class TestGithubOrgClient(unittest.TestCase):
         mocked_get_json.assert_called_with(
             github_org.ORG_URL.format(org=org_name))
         mocked_get_json.assert_called_once()
+
+    def test_public_repos_url(self):
+        """test public repos url"""
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as mocked_prop:
+            mocked_prop.return_value = {"repos_url": "www"}
+            result = GithubOrgClient("Google")._public_repos_url
+            self.assertEqual(result, "www")
