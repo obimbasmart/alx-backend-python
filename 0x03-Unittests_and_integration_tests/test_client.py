5cf83b0758ac474ff06c6a64b53dd9b7c3f160bc
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-04-10 00:53:13 +0100

    test: has_license_key

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -47,3 +47,12 @@ class TestGithubOrgClient(unittest.TestCase):
             mocked_prop.return_value = "example.com"
             result = GithubOrgClient("Google").public_repos()
             self.assertEqual(result, ["NBC", "PG"])
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo: Dict, license_key: str, expected: bool):
+        """--- --- ---"""
+        result = GithubOrgClient("google").has_license(repo, license_key)
+        self.assertEqual(result, expected)
